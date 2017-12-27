adb871d2297bdac45fd320ef018551108ebab46d
exemplaryer <exemplaryer@gmail.com>
2017-12-27 13:22:14 +0900

    자바 3장까지 코드작성한 것들

---
+++ ch01/src/ch01/HelloJava.java
@@ -0,0 +1,9 @@
+package ch01;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("자바의 첫 프로그램 입니다.");
+	}
+
+}
---
+++ ch02/src/programming/basic/ToCode.java
@@ -0,0 +1,11 @@
+package programming.basic;
+
+public class ToCode {
+	
+	public static void main(String[] args) {
+		
+		char character = '\uc790';	// 'ÀÚ'
+		System.out.println(character);
+		System.out.printf("%c", character);
+	}
+}
