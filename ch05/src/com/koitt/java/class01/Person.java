73d3e75ea512423f45b7a78953890685f8792dab
exemplaryer <exemplaryer@gmail.com>
2018-01-02 11:23:49 +0900

    5장 클래스의 다형성 설명 (153쪽)

--- ch05/src/com/koitt/java/class01/Person.java
+++ ch05/src/com/koitt/java/class01/Person.java
@@ -25,4 +25,19 @@ public class Person extends Animal {
 	public void setAddress(String address) {
 		this.address = address;
 	}
+	
+	// 메소드 - Person의 내용을 출력
+	public void print() {
+		System.out.println("주민등록번호: " + this.ssn + ", 주소: " + this.address);
+	}
 }
+
+
+
+
+
+
+
+
+
+
