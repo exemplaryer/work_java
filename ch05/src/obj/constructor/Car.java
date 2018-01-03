97e457a0dcb1b52247299b92988fa6870be42a33
exemplaryer <exemplaryer@gmail.com>
2018-01-03 10:04:19 +0900

    5장 실습예제 5-10 개선 후

--- ch05/src/obj/constructor/Car.java
+++ ch05/src/obj/constructor/Car.java
@@ -2,8 +2,8 @@ package obj.constructor;
 
 public class Car {
 	private int maxSpeed;		// 최고속도
-	public String brandName;	// 브랜드이름
-	public int speed;			// 현재 속도
+	private String brandName;	// 브랜드이름
+	private int speed;			// 현재 속도
 	
 	// 브랜드 이름을 지정하는 생성자
 	public Car(String brandName) {
@@ -18,11 +18,33 @@ public class Car {
 	
 	// 속도 관련 메소드
 	public int speedUp() {
-		return this.speed += 30;
+		int tempSpeed = this.speed;
+		tempSpeed += 30;
+		
+		// 최대 속도보다 더 높다면 이전 속도 유지
+		if (tempSpeed > this.maxSpeed) {
+			return this.speed;
+		}
+		// 최대 속도보다 같거나 작다면 현재 속도를 갱신
+		this.speed = tempSpeed;
+		
+		return this.speed;
 	}
 	
 	public int speedDown() {
-		return this.speed -= 20;
+		// 현재 속도를 임시 저장
+		int tempSpeed = this.speed;
+		tempSpeed -= 20;
+		
+		// 현재 속도가 0보다 작다면
+		if (tempSpeed < 0) {
+			return 0;
+		}
+		
+		// 현재 속도를 계산한 속도로 갱신
+		this.speed = tempSpeed;
+		
+		return this.speed;
 	}
 	
 	// maxSpeed의 setter와 getter
