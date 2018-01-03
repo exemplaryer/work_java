2ecae13505967df93ad601071b2a1e3af3327dfc
exemplaryer <exemplaryer@gmail.com>
2018-01-03 15:13:22 +0900

    5장 프로그래밍 연습 2번

--- ch05/src/practice/Student.java
+++ ch05/src/practice/Student.java
@@ -2,8 +2,8 @@ package practice;
 
 public class Student {
 	
-	String dept;
-	int studentId;
+	private String dept;
+	private int studentId;
 	
 	public static void main(String[] args) {
 		Student s1 = new Student();
@@ -11,5 +11,27 @@ public class Student {
 		s1.studentId = 100;
 		
 		System.out.println("학과: " + s1.dept + ", 학번: " + s1.studentId);
+		
+		Student s2 = new Student();
+		s2.setDept("연기과");
+		s2.setStudentId(200);
+		
+		System.out.println("학과: " + s2.getDept() + ", 학번: " + s2.getStudentId());
+	}
+
+	public String getDept() {
+		return dept;
+	}
+
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
 	}
 }
