2ff2066d4f13fb324ad807082d32656765bb11d5
exemplaryer <exemplaryer@gmail.com>
2018-01-04 14:48:40 +0900

    6장 실습예제 6-4, 6-5 개선 후

--- ch06/src/inheritance/constructor/Vehicle.java
+++ ch06/src/inheritance/constructor/Vehicle.java
@@ -1,9 +1,9 @@
 package inheritance.constructor;
 
 public class Vehicle {
-	public String name = "차량";	// 이름
-	public double maxSpeed;			// 최대속도
-	public int seater;				// 정원
+	private String name = "차량";	// 이름
+	private double maxSpeed;		// 최대속도
+	private int seater;				// 정원
 	
 	// 기본 생성자
 	public Vehicle() {
@@ -13,6 +13,31 @@ public class Vehicle {
 	// 생성자
 	public Vehicle(double maxSpeed, int seater) {
 		this.maxSpeed = maxSpeed;
+		this.seater = seater;
+	}
+
+	// getter, setter
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setMaxSpeed(double maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public int getSeater() {
+		return seater;
+	}
+
+	public void setSeater(int seater) {
 		this.seater = seater;
 	}
 }
