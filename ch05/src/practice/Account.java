55b123c6cb7892e4f2df2cd17b3bc6c1fbddc59c
exemplaryer <exemplaryer@gmail.com>
2018-01-04 12:04:33 +0900

    5장 연습문제 7번

--- ch05/src/practice/Account.java
+++ ch05/src/practice/Account.java
@@ -27,4 +27,18 @@ public class Account {
 		this.balance = balance;
 	}
 	
+	// 7.
+	public long deposit(long amount) {
+		return this.balance += amount;
+	}
+	
+	public long withdraw(long amount) {
+		return this.balance -= amount;
+	}
+	
+	public static void main(String[] args) {
+		Account account = new Account("홍길동", 1000000);
+		System.out.println("현재 잔액: " + account.deposit(1500000));
+		System.out.println("현재 잔액: " + account.withdraw(50000));
+	}
 }
