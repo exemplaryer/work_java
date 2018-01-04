fac92935676ffca40d20ccc16a7cb170f43c9e21
exemplaryer <exemplaryer@gmail.com>
2018-01-04 11:47:07 +0900

    5장 프로그래밍 연습 4번 풀이

--- ch05/src/practice/Cylinder.java
+++ ch05/src/practice/Cylinder.java
@@ -6,6 +6,12 @@ public class Cylinder {
 	Circle circle;
 	double height;
 	
+	// 4. 1번째 지문
+	public Cylinder(Circle circle, double height) {
+		this.circle = circle;
+		this.height = height;
+	}
+	
 	public double getVolume() {
 		double area = circle.getArea();	// 원 넓이를 구한다.
 		double volume = area * this.height;
@@ -23,12 +29,12 @@ public class Cylinder {
 	
 	public static void main(String[] args) {
 		// 1. 원을 만든다. (Circle 클래스를 이용하여 객체를 생성한다.)
-		Circle c1 = new Circle(2.8);
+		//Circle c1 = new Circle(2.8);
 		
 		// 2. 실린더를 만든다. (Cylinder 클래스를 이용하여 객체를 생성한다.)
-		Cylinder cylinder = new Cylinder();
-		cylinder.setCircle(c1);		// cylinder 객체의 circle 필드를 초기화
-		cylinder.setHeight(5.6); 	// cylinder 객체의 height 필드를 초기화
+		Cylinder cylinder = new Cylinder(new Circle(2.8), 5.6);
+		//cylinder.setCircle(c1);		// cylinder 객체의 circle 필드를 초기화
+		//cylinder.setHeight(5.6); 	// cylinder 객체의 height 필드를 초기화
 		
 		System.out.println(cylinder.circle + " / " + cylinder.height);
 		
