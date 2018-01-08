f87892fd16c95e04ba7f1b91691ec00a90ee9f7a
exemplaryer <exemplaryer@gmail.com>
2018-01-08 14:04:16 +0900

    DI (Dependency Injection) 의존성 주입 설명 예제

---
+++ ch06/src/com/koitt/java/di/DBFactory.java
@@ -0,0 +1,17 @@
+package com.koitt.java.di;
+
+public class DBFactory {
+	
+	public static KoittDB getDB(String type) {
+		
+		switch (type) {
+			case "Ora":
+				return new OraDB();
+				
+			case "MyS":
+				return new MySADB();
+		}
+		
+		return null;
+	}
+}
