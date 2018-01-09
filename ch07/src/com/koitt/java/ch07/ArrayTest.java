798213b7c41f04f5d42234bb2a87c685d6be82bb
exemplaryer <exemplaryer@gmail.com>
2018-01-09 15:14:53 +0900

    7장 283쪽 실습예제 7-13까지 (제네릭 예제 설명전까지)

--- ch07/src/com/koitt/java/ch07/ArrayTest.java
+++ ch07/src/com/koitt/java/ch07/ArrayTest.java
@@ -3,43 +3,43 @@ package com.koitt.java.ch07;
 public class ArrayTest {
 	
 	private Integer[] array;	// 데이터를 담는 자료구조
-	private int size;			// 배열의 크기
-	private int length;			// 실제 데이터의 개수
+	private int length;			// 배열의 크기
+	private int size;			// 실제 데이터의 개수
 	
 	public ArrayTest(int size) {
-		this.size = size;
-		this.array = new Integer[this.size];
+		this.length = size;
+		this.array = new Integer[this.length];
 	}
 	
 	// 자료구조에 데이터를 추가 (index 번호에다가 해당 데이터를 저장)
 	public void add(int index, int value) {
-		if (index >= 0 && index < this.size) {
+		if (index >= 0 && index < this.length) {
 			if (this.array[index] == null) {
 				this.array[index] = value;
 			}
 			else if (this.array[index] != null) {
-				int newSize = this.size + 1;
+				int newSize = this.length + 1;
 				if (this.array.length < newSize) {
 					// 깊은복사(Deep copy): 기존 데이터를 손상없이 보존하기 위해서 임시로 다른 배열에 값을 복사
-					Integer[] tempArray = new Integer[this.size];
+					Integer[] tempArray = new Integer[this.length];
 					System.arraycopy(this.array, 0, tempArray, 0, this.array.length);
 					
 					// 
-					this.size *= 2;
-					this.array = new Integer[this.size];
+					this.length *= 2;
+					this.array = new Integer[this.length];
 					System.arraycopy(tempArray, 0, this.array, 0, tempArray.length);
 				}
 				
 				// 집어 넣으려는 index번호 뒤쪽 데이터를 한칸씩 뒤로 이동
-				System.arraycopy(this.array, index, this.array, index + 1, this.length - index);
+				System.arraycopy(this.array, index, this.array, index + 1, this.size - index);
 				this.array[index] = value;	// 새로 추가하려는 값을 배열에 저장
 			}
-			this.length += 1;	// 기존 데이터 개수 + 새로운 데이터 개수
+			this.size += 1;	// 기존 데이터 개수 + 새로운 데이터 개수
 		}
 	}
 	
 	public void remove(int index) {
-		if (index >= 0 && index < this.size) {
+		if (index >= 0 && index < this.length) {
 			
 		}
 	}
@@ -56,33 +56,33 @@ public class ArrayTest {
 		return array;
 	}
 	
-	public int getLength() {
-		return length;
+	public int getSize() {
+		return size;
 	}
 
 	public static void main(String[] args) {
 		ArrayTest test = new ArrayTest(3);
 		
 		test.add(0, 2);
-		for (int i = 0; i < test.getLength(); i++) {
+		for (int i = 0; i < test.getSize(); i++) {
 			System.out.print(test.get(i));
 		}
 		System.out.println();
 		
 		test.add(1, 3);
-		for (int i = 0; i < test.getLength(); i++) {
+		for (int i = 0; i < test.getSize(); i++) {
 			System.out.print(test.get(i));
 		}
 		System.out.println();
 		
 		test.add(2, 4);
-		for (int i = 0; i < test.getLength(); i++) {
+		for (int i = 0; i < test.getSize(); i++) {
 			System.out.print(test.get(i));
 		}
 		System.out.println();
 		
 		test.add(1, 1);
-		for (int i = 0; i < test.getLength(); i++) {
+		for (int i = 0; i < test.getSize(); i++) {
 			System.out.print(test.get(i));
 		}
 		System.out.println();
