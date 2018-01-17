cbde29d8a0cb00b93139c70c0578269a46f09bf1
exemplaryer <exemplaryer@gmail.com>
2018-01-17 17:26:47 +0900

    8장 337쪽까지

---
+++ ch08/src/wait/TestDrive.java
@@ -0,0 +1,11 @@
+package wait;
+
+import java.util.Random;
+
+public class TestDrive {
+	
+	public static void main(String[] args) {
+		double nextDouble = new Random().nextDouble() * 5 + 1;
+		System.out.println(nextDouble);
+	}
+}
