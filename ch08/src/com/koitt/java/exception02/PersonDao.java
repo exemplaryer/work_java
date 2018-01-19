8f44826d7858bdf538caa57bb56b208696c7d83e
exemplaryer <exemplaryer@gmail.com>
2018-01-19 15:14:21 +0900

    인적사항 관리 프로그램 - 파일 입출력 기능 구현

--- ch08/src/com/koitt/java/exception02/PersonDao.java
+++ ch08/src/com/koitt/java/exception02/PersonDao.java
@@ -1,5 +1,11 @@
 package com.koitt.java.exception02;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,10 +16,14 @@ import java.util.List;
  */
 
 public class PersonDao {
+	public static final String FILE_NAME = "person-list.dat"; 
+	
 	List<Person> list;	// DB 대신에 사용자를 저장할 자료구조
 
 	public PersonDao() {
-		this.list = new ArrayList<Person>();
+		// this.list = new ArrayList<Person>();
+		// TODO 6. this.list = loadFromFile([파일명]);
+		this.list = loadFromFile(PersonDao.FILE_NAME);
 	}
 	
 	public void insert(Person p) throws MyException {
@@ -26,6 +36,8 @@ public class PersonDao {
 		}
 			
 		this.list.add(p);
+		// TODO 3. saveToFile(list, [파일명]);
+		this.saveToFile(this.list, PersonDao.FILE_NAME);
 	}
 	
 	// 전체 목록 가져오기
@@ -46,6 +58,8 @@ public class PersonDao {
 		for (int i = 0; i < this.list.size(); i++) {
 			if (this.list.get(i).equals(p)) {
 				this.list.remove(i);
+				// TODO 4. saveToFile(list, [파일명]);
+				this.saveToFile(this.list, PersonDao.FILE_NAME);
 				return;
 			}
 		}
@@ -59,10 +73,71 @@ public class PersonDao {
 			if (item.equals(p)) {
 				//item.setName(p.getName());	// name을 새로 set할 필요가 없다.
 				item.setAge(p.getAge());
+				// TODO 5. saveToFile(list, [파일명]);
+				this.saveToFile(this.list, PersonDao.FILE_NAME);
 				return;	// 찾았으니까 검색을 중단하고 끝낸다.
 			}
 		}
 		
 		throw new MyException("E03: 수정할 사람이 없습니다.");
 	}
+	
+	/*
+	 * TODO 1. private void saveToFile(List<Person> list, String filename)
+	 * list: PersonDao에 있는 list 필드
+	 * filename: list 객체를 저장할 파일 이름
+	 */
+	private void saveToFile(List<Person> list, String filename) {
+		// FileOutputStream, ObjectOutputStream
+		FileOutputStream fos = null;
+		ObjectOutputStream oos = null;
+		
+		try {
+			fos = new FileOutputStream(filename);
+			oos = new ObjectOutputStream(fos);
+			
+			oos.writeObject(list);
+			
+			oos.close();
+			fos.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * TODO 2. private List<Person> loadFromFile(String filename)
+	 * filename: 게시글 리스트가 저장된 파일명
+	 * 
+	 * List<Person> : 파일에서 불러온 list 객체를 리턴
+	 */
+	private List<Person> loadFromFile(String filename) {
+		FileInputStream fis = null;
+		ObjectInputStream ois = null;
+		Object list = null;
+		
+		try {
+			fis = new FileInputStream(filename);
+			ois = new ObjectInputStream(fis);
+			
+			// 파일에 있던 리스트 객체를 끄집어내기
+			list = ois.readObject();
+			
+			ois.close();
+			fis.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			return (List<Person>) list;
+		}
+	}
+	
 }
