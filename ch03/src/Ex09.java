6bceac7e7be4a7db77276e3298bdd4a87961a2dc
exemplaryer <exemplaryer@gmail.com>
2018-06-04 16:18:32 +0900

    3장 연습문제

---
+++ ch03/src/Ex09.java
@@ -0,0 +1,8 @@
+
+public class Ex09 {
+	public static void main(String[] args) {
+		char ch = 'z';
+		boolean b = ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ('0' <= ch && ch <= '9');
+		System.out.println(b);
+	}
+}
