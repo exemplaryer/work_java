97f0a4e9cd277fb90a151019b27b5e66108078fe
exemplaryer <exemplaryer@gmail.com>
2018-12-30 03:38:59 +0900

    teachertophoon 계정 코드 merge

--- ch01/src/ch01/Hello.java
+++ ch01/src/ch01/Hello.java
@@ -3,7 +3,7 @@ package ch01;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, world.");
-		System.out.println("¾È³ç, ¼¼°è.");
-		System.out.println("Á¤»óÈÆ");
+		System.out.println("ì•ˆë…•, ì„¸ê³„.");
+		System.out.println("ì •ìƒí›ˆ");
 	} 
 }
